b792eb60eb71d96ec85a76980cd4397b619612b4
Jorge Junior <jorgeluiz.sjr@outlook.com>
2019-10-18 13:08:26 -0300

    primeiro commit aula variaveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+//var variavel1 = "valor pode mudar"
+//var variavel1 = "conteudo 2"
+
+//console.log(variavel1)
+
+let variavelLet = "valor pode mudar"
+console.log(variavelLet)
+
+variavelLet = "outro valor"
+console.log(variavelLet)
+
+
+const variavelConst = "valor não pode mudar"
+
+// gerar erro
+//variavelConst = "tentando setar outro valor"
+
+console.log(variavelConst)
